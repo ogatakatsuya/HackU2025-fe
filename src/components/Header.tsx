f66ddb48a7387adb65b3bac6881ecbdf0d699fe1
Pictogram23 <shanchengyinger@gmail.com>
2025-03-08 11:49:49 +0000

    ヘッダーにルートに戻るボタンを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,31 +3,42 @@
 import AccountButton from "@/components/AccountButton";
 import { useAppStateContext } from "@/components/Context";
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+	AppBar,
+	Box,
+	Button,
+	IconButton,
+	Toolbar,
+	Typography,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
 	const { user } = useAppStateContext();
+	const router = useRouter();
 
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar>
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						おもえも(仮)
-					</Typography>
-					<AccountButton user={user} />
-				</Toolbar>
-			</AppBar>
-		</Box>
+		<AppBar>
+			<Toolbar>
+				<IconButton
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="menu"
+					sx={{ mr: 2 }}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Box sx={{ flexGrow: 1 }}>
+					<Button color="inherit" onClick={() => router.push("/")}>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							おもえも(仮)
+						</Typography>
+					</Button>
+				</Box>
+				<AccountButton user={user} />
+			</Toolbar>
+		</AppBar>
 	);
 };
 
